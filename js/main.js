96e7301f50613bf9c667a9485a6bb1797c741db9
Dom\Ivan <nt-polza@yandex.ru>
2021-05-03 13:45:39 +0500

    Функционал изменения пункта

--- js/main.js
+++ js/main.js
@@ -36,15 +36,13 @@ $(".buttAdd").on("click", ()=>{
   arrDo.push({id:arrDo.length+1,text:$("#inputText").val()});
   $("#inputText").val("");
 
-  $(".list").html($(".list").html()
-  +"<li id='pn" +(arrDo.length) + "'> <div class='container'> <div class='visuDisplay' name='point'>"
-  + arrDo[arrDo.length-1].text + "</div> <div class='noneDisplay'> <label title='Введите текст. Нажмите Enter |&crarr;|'>  <input type='text' size='30'> </label> </div> <div class='buttons'> <button  onclick='deleteDo(event)'>Удалить</button> <button onclick='buttEdit(event)'>Изменить</button> </div> </div> </li>");
+  updateList();
 })
 
 //Удаление пунтка
 function deleteDo(event) {
   let li = event.path[3];
-  n = Number(li.id.slice(2));
+  let n = Number(li.id.slice(2));
   let newArr =[];
   let i = 1;
   arrDo.forEach( item => {
@@ -61,8 +59,8 @@ function updateList(){
   $('ol').empty();
   arrDo.forEach((item, i) => {
     $(".list").html($(".list").html()
-    +"<li id='pn" +(i+1) + "'> <div class='container'> <div class='visuDisplay' name='point'>"
-    + item.text + "</div> <div class='noneDisplay'> <label title='Введите текст. Нажмите Enter |&crarr;|'>  <input type='text' size='30'> </label> </div> <div class='buttons'> <button  onclick='deleteDo(event)'>Удалить</button> <button onclick='buttEdit(event)'>Изменить</button> </div> </div> </li>");
+    +"<li id='pn" +(i+1) + "'> <div class='container'> <div class='visuDisplay point'>"
+    + item.text + "</div> <div class='noneDisplay divinput'> <label title='Введите текст. Нажмите Enter |&crarr;|'>  <input id='in" + (i+1)+ "' type='text' size='30'> </label> </div> <div class='buttons'> <button  onclick='deleteDo(event)'>Удалить</button> <button onclick='buttEdit(event)'>Изменить</button> </div> </div> </li>");
   });
 }
 
@@ -75,8 +73,37 @@ $("#seveLS").on("click",()=>{
 
 //Изменить пункт
 function buttEdit(event) {
-  let buf = event.path[2];
-  let el = buf.getElementsByName("point")[0];
-  console.log(buf);
-  console.log(el);
+  let buf = event.path[3];
+  let id = "#" + buf.id;
+  let n = Number(buf.id.slice(2));
+  console.log(id);
+  $(id).find(".container").find(".point").attr("class","noneDisplay point");
+  $(id).find(".container").find(".divinput").attr("class","visuDisplay divinput");
+  $(id).find(".container").find("input").val(arrDo[n-1].text);
 }
+
+//Применить изменения
+$(".list").delegate("input","keydown",function (event) {
+  if(event.key ==="Enter") {
+      let n = Number($(this).attr("id").slice(2));
+      let id = "#pn" + n;
+      arrDo[n-1].text = $(this).val();
+      $(id).find(".container").find(".point").attr("class","visuDisplay point");
+      $(id).find(".container").find(".divinput").attr("class","noneDisplay divinput");
+      updateList();
+  }
+});
+
+// $(".change").keyup(function(e){
+//     alert("Entered!");
+//     var code = e.key; // recommended to use e.key, it's normalized across devices and languages
+//     if(code==="Enter") e.preventDefault();
+//     if(code==="Enter"){
+//         //$("#displaysomething").html($(this).val());
+//     } // missing closing if brace
+// });
+
+// $(document).on( "keydown", function(event) {
+//     if(event.which == 13)
+//         alert("Entered!");
+// });
